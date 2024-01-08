7d13825c3e6c8deb5b387dc9a420a6ac75dccf8e
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-01-09 04:17:35 +0600

    init compose project

--- app/src/main/java/com/hossain/sharedelementtransitionandroid/MainActivity.kt
+++ app/src/main/java/com/hossain/sharedelementtransitionandroid/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.hossain.sharedelementtransitionandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hossain.sharedelementtransitionandroid.ui.theme.SharedElementTransitionAndroidTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            SharedElementTransitionAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SharedElementTransitionAndroidTheme {
+        Greeting("Android")
     }
 }
